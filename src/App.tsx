3ee9c0898a01ad526981106cc2cd6b5319bdd37b
Omar <omarrian3333@gmail.com>
2025-10-19 10:34:59 +0100

    nothing

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import gsap from 'gsap'
   gsap.registerPlugin(ScrollTrigger, SplitText)
    return (
      <div className="flex-center h-screen">
-       <h1 className="text-3xl text-indigo-530">Hello, Gsap!!</h1>
+       <h1 className="text-3xl text-indigo-530">Hello, Gsap!</h1>
      </div>
    )
  }
