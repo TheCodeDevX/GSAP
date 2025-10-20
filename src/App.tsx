ed15608b4a58d1692d4ad835a1a8849a0f774ce4
Omar <omarrian3333@gmail.com>
2025-10-20 13:03:00 +0100

    Implement Menu Section

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Hero from "./components/Hero"
 import Cocktails from "./components/Cocktails"
 import About from "./components/About"
 import Art from "./components/Art"
+import Menu from "./components/Menu"
 
  const App = () => {
   gsap.registerPlugin(ScrollTrigger, SplitText)
@@ -16,6 +17,7 @@ import Art from "./components/Art"
       <Cocktails/>
       <About/>
       <Art/>
+      <Menu/>
      </main>
    )
  }
